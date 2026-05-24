0fb3de446a5886af1e581c58eb353863fe86701e
fawazmalick-hub <fawazmalick@gmail.com>
2026-05-24 08:15:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.messages = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
